48a29b7c4687b9bfeafa3a6d15d9d884b681f8dc
jcharbo <jeremey@doctornetwork.com>
2017-02-20 14:25:59 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,21 @@ import { NativeModules, PermissionsAndroid, Platform } from 'react-native'
 
 async function send(options: Object, callback: () => void) {
   if (Platform.OS === 'android') {
-    try {
-      let authorized = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_SMS)
-    } catch (error) {
-
+     try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.READ_SMS,
+        {
+          'title': 'The Jump Needs Your Permission',
+          'message': 'You have requested to send a text message so we need you to grant us permission.'
+        }
+      )
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log("You can use the camera")
+      } else {
+        console.log("Camera permission denied")
+      }
+    } catch (err) {
+      console.warn(err)
     }
   }
   NativeModules.SendSMS.send(options, callback);
